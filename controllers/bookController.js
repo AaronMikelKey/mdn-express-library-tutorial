b54bc4ea4789a352f88e64b396cddea44bfaf753
buckshot307 <buckshot307@gmail.com>
2020-10-22 13:36:29 -0400

    Added book list page and controller function

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -30,7 +30,13 @@ exports.index = function(req, res) {
 
 //display list of books
 exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: book list');
+    Book.find({}, 'title author')
+    .populate('author')
+    .exec(function (err, list_books) {
+        if (err) {return next(err); }
+        //Success, then render
+        res.render('book_list', { title: 'Book List', book_list: list_books });
+    });
 };
 
 //display detail for a specific book
