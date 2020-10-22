c0a7b66071447cc2f36e8e0b10240f375f9b2a4c
buckshot307 <buckshot307@gmail.com>
2020-10-22 18:41:22 -0400

    Added controller and view for new author_form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 var async = require('async');
 var Author = require('../models/author');
 var Book = require('../models/book');
+const { body,validationResult } = require('express-validator');
 
 //display list of authors
 exports.author_list = function(req, res, next) {
@@ -40,13 +41,50 @@ exports.author_detail = function(req, res, next) {
 
 //Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', { title: 'Create Author' });
 };
 
 //Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+
+    //Validate and sanitize fields
+    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters'),
+    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+    //Process request after validation and sanitization
+    (req, res, next) => {
+
+        //Extract the validation errors
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            //There are errors, rerender form with sanitized values and errors
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            //Data form is valid:
+
+            //Create an Author object with escaped and trimmed data
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                });
+            author.save(function (err) {
+                if (err) { return next(err); }
+                //Succesful - redirect to new author record.
+                res.redirect(author.url);
+            });
+        }
+    }
+];
 
 //Display Author delete form on GET
 exports.author_delete_get = function(req, res) {
