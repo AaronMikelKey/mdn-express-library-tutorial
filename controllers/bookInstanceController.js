7b0bc1b7ae13bed343e59de3ee3ff761c80cc1d7
buckshot307 <buckshot307@gmail.com>
2020-10-22 13:52:40 -0400

    Added page for book instances.  Added luxon for date formatting to package but haven't implemented yet

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,8 +1,15 @@
-var Bookinstance = require('../models/bookinstance');
+var BookInstance = require('../models/bookinstance');
 
 //display list of bookinstances
-exports.bookinstance_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: bookinstance list');
+exports.bookinstance_list = function(req, res, next) {
+    
+    BookInstance.find()
+    .populate('book')
+    .exec(function (err, list_bookinstances) {
+        if (err) {return next(err); }
+        //Success, then render
+        res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
+    });
 };
 
 //display detail for a specific bookinstance
