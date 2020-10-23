a37f16e710708dd2b9915241bfeac9a04718deed
buckshot307 <buckshot307@gmail.com>
2020-10-22 21:45:29 -0400

    Added new book_form and controller, fixed missing route in catalog

--- routes/catalog.js
+++ routes/catalog.js
@@ -12,7 +12,10 @@ var book_instance_controller = require('../controllers/bookInstanceController');
 // GET catalog home page
 router.get('/', book_controller.index);
 
-//POST request for for creating a Book.  NOTE This must come before routes that display Book (uses id)
+//GET request for for creating a Book.  NOTE This must come before routes that display Book (uses id)
+router.get('/book/create', book_controller.book_create_get);
+
+// POST request for creating Book.
 router.post('/book/create', book_controller.book_create_post);
 
 //GET request to delete Book
